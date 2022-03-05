6bf5c0607ff9319ba194ab1e8a8e160028f21aac
Matthew Derek <mamaba@human-incubator.com>
2022-03-05 16:26:28 +0800

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class UserController extends Controller
+{
+    public function index(){
+		return response()->json(User::all());
+	}
+
+	public function login(){
+		$email = 'gaylord15@example.org';
+		$password = 'password';
+
+		if(Auth::attempt(['email' => $email, 'password' => $password])){
+			return 'ok';
+		}else{
+			return 'not ok';
+		}
+	}
+
+}
